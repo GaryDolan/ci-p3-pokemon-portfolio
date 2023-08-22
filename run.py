bf41ac8145ed646453af34073b104ea1d6464e28
GaryDolan <garydolan4@gmail.com>
2023-08-22 03:27:50 +0100

    Get and validate password and phone number

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 # ------------------------- LIBRARY IMPORTS ---------------------------
-# Created by me to store and print pokemon ascii art 
+# Created by me to store and print pokemon ascii art
 from pokemon_ascii_art import print_pokemon
 
 # Creates text-based ASCII art banners
@@ -29,11 +29,11 @@ from google.oauth2.service_account import Credentials
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+    "https://www.googleapis.com/auth/drive",
+]
 
 # Create a Credentials instance from a service account json file
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file("creds.json")
 
 # Create a copy of the credentials with specified scope
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -42,7 +42,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
 # Access sheet for project
-SHEET = GSPREAD_CLIENT.open('pokemon_portfolio')
+SHEET = GSPREAD_CLIENT.open("pokemon_portfolio")
 
 # login = SHEET.worksheet('login')
 
@@ -52,37 +52,42 @@ SHEET = GSPREAD_CLIENT.open('pokemon_portfolio')
 # --------------------------- CLASSES -----------------------------
 
 
-
-
 # --------------------- APP LOGIC FUNCTIONS -----------------------
 
-def display_welcome_banner():
-    '''
-    Displays welcome banner and image 
 
-    Parameters: 
+def display_welcome_banner():
+    """
+    Displays welcome banner and image
+
+    Parameters:
         None
-    Returns: 
+    Returns:
         None
-    '''
+    """
     print_art_font("Pokemon Portfolio")
 
     print_pokemon("pikachu_banner")
 
     login_options()
 
+
 def login_options():
     """
     Displays the login opitions to user.
-    Takes user selection, validates and calls appropiate function.  
+    Takes user selection, validates and calls appropiate function.
 
-    Parameters: 
+    Parameters:
         None
-    Returns: 
+    Returns:
         None
     """
     while True:
-        print_center_string(colored("Please select an option (1-3) from the list shown and enter it below\n", attrs = ['bold', 'underline']))
+        print_center_string(
+            colored(
+                "Please select an option (1-3) from the list shown and enter it below\n",
+                attrs=["bold", "underline"],
+            )
+        )
 
         print("1. Log into your account")
         print("2. Create an account")
@@ -102,25 +107,35 @@ def login_options():
             password_recovery()
             break
 
+
 def account_login():
     clear_terminal()
     print_art_font("        Account Login")
+
 
 def create_account():
     clear_terminal()
     print_art_font(" Account Creation")
 
     print("\n\n\n")
-    print_center_string(colored("Please follow the steps below to create an account\n", attrs = ['bold', 'underline']))
+    print_center_string(
+        colored(
+            "Please follow the steps below to create an account\n",
+            attrs=["bold", "underline"],
+        )
+    )
 
     username = get_valid_username()
-    
-    # ad this line in once I confirm that the username is not taken in gsheets, if taken, print taken and call get valid username again
-    #print_center_string(colored("Username available\n", 'green'))
 
-    #password = get_valid_password()
-    #phone_num = get_valid_phone_num()
-        
+    # ad this line in once I confirm that the username is not taken in gsheets, if taken, print taken and call get valid username again
+    # print_center_string(colored("Username available\n", 'green'))
+
+    password = get_valid_password()
+
+    phone_num = get_valid_phone_num()
+    # add this after I validate that phone num is not in gsheets already
+    # print_center_string(colored("Username available\n", 'green'))
+
 
 def password_recovery():
     clear_terminal()
@@ -129,30 +144,32 @@ def password_recovery():
 
 # ----------------------- HELPER FUNCTIONS ------------------------
 
+
 def print_art_font(string):
     """
     Uses pyfiglet library to convert given string into an art font style
-   
-    Parameters: 
+
+    Parameters:
         text (string): Text to be converted
-    Returns: 
+    Returns:
         None
     """
     font = pyf.Figlet(font="big", width=110)
     msg = font.renderText(string)
     msg = msg.rstrip()
-    print(msg)           
-   
+    print(msg)
+
+
 def print_center_string(string):
     """
-    Centers and prints the given text to the terminal 
+    Centers and prints the given text to the terminal
     If text contains ascii escape codes for color etc
-    the function will stip these out for calculating 
-    spacing but will still print original text 
+    the function will stip these out for calculating
+    spacing but will still print original text
 
-    Parameters: 
+    Parameters:
         String (string): String to be centered and printed
-    Returns: 
+    Returns:
         None
     """
 
@@ -161,9 +178,10 @@ def print_center_string(string):
     # If string contains ascii escape chars, use re to clear them before calculations
     processed_string = re.sub(r"(\x1b|\033)\[[0-9;]*m", "", string)
 
-    spaces = int((terminal_width - len(processed_string)) /2)
+    spaces = int((terminal_width - len(processed_string)) / 2)
     centered_string = " " * spaces + string
     print(centered_string)
+
 
 def clear_terminal():
     if os.name == "posix":  # Linux and macOS
@@ -171,7 +189,9 @@ def clear_terminal():
     elif os.name == "nt":  # Windows
         os.system("cls")
 
+
 # --------------------- VALIDATION FUNCTIONS ----------------------
+
 
 def validate_selection(selection_str, available_choices):
     """
@@ -179,55 +199,109 @@ def validate_selection(selection_str, available_choices):
     Validates that it can be converted to an int
     Also validates that user selection was one of the available choices
 
-    Parameters: 
+    Parameters:
         selection_str (string): User selection to be validated
-        available_choices (list): List of choices available to user 
-    Returns: 
+        available_choices (list): List of choices available to user
+    Returns:
         int or False: Returns selection value as an int if valid otherwise returns False
     """
     try:
         selection_value = int(selection_str)
         if selection_value not in available_choices:
-            raise ValueError(f"Available options ({available_choices[0]} - {available_choices[-1]}), you entered {selection_value}")
+            raise ValueError(
+                f"Available options ({available_choices[0]} - {available_choices[-1]}), you entered {selection_value}"
+            )
     except ValueError as e:
-        print_center_string(colored(f"Invalid selection: {e}, please try again\n", 'red'))
+        print()
+        print_center_string(
+            colored(f"Invalid selection: {e}, please try again\n", "red")
+        )
         return False
     return selection_value
+
 
 def get_valid_username():
     while True:
         try:
-            # print("Please enter a username between 5 and 15 characters long\n")
-            username = input("Please enter a username between 5 and 15 characters long,\n(You may user letters, numbers, _ or -) : ")
+            username = input(
+                "\nPlease enter a username between 5 and 15 characters long,\n(You may user letters, numbers, _ or -) : "
+            )
 
             if len(username) < 5:
                 raise ValueError("Username must be at least 5 characters")
-            
+
             if len(username) > 15:
                 raise ValueError("Username can not be more than 15 characters")
             if not re.match("^[a-zA-Z0-9_-]*$", username):
                 raise ValueError("Username can only use letters, numbers, _ or -")
-             
-            return (username)
-        
+
+            return username
 
         except ValueError as e:
-            print_center_string(colored(f"Invalid username: {e}, please try again\n", 'red'))
+            print("")
+            print_center_string(
+                colored(f"Invalid username: {e}, please try again\n", "red")
+            )
 
-        
 
 def get_valid_password():
-    pass
+    while True:
+        try:
+            password = input(
+                "\nPlease enter a password between 5 and 15 characters long,\n(You may user letters, numbers, _ , - , & or !) : "
+            )
+
+            if len(password) < 5:
+                raise ValueError("Password must be at least 5 characters")
+
+            if len(password) > 15:
+                raise ValueError("Password cannot be more than 15 characters")
+
+            if not re.match("^[a-zA-Z0-9_&!-]*$", password):
+                raise ValueError("Please only use letters, numbers, _ , - , & or !")
+
+            return password
+
+        except ValueError as e:
+            print("")
+            print_center_string(
+                colored(f"Invalid Password: {e}, please try again\n", "red")
+            )
+
 
 def get_valid_phone_num():
-    pass
+    while True:
+        try:
+            phone_num = input(
+                "\nPlease enter a mobile phone number consisting of 10 to 15 digits: "
+            )
+
+            if len(phone_num) < 10:
+                raise ValueError("Phone number must be at least 10 digits")
+
+            if len(phone_num) > 15:
+                raise ValueError("Phone number cannot be more than 15 digits")
+
+            if not re.match("^[0-9]*$", phone_num):
+                raise ValueError("Please only use numbers")
+
+            return phone_num
+
+        except ValueError as e:
+            print("")
+            print_center_string(
+                colored(f"Invalid phone number: {e}, please try again\n", "red")
+            )
+
 
 # ----------------------------- MAIN -------------------------------
+
 
 def main():
     """
     Run Pokemon Portfolio terminal application
     """
     display_welcome_banner()
+
 
 main()
